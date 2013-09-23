2c6dfe66c760e810c9a1a73a223630aa798f569b
Daniel Duckworth <dux@premise.com>
2013-09-23 13:35:11 -0700

    bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'sqlrest',
-    version = '0.1.3',
+    version = '0.1.4',
     author = 'Daniel Duckworth',
     author_email = 'duckworthd@gmail.com',
     description = "Instant REST APIs for SQL tables",
